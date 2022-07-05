ff2291bfa8f90e4bf24aa483d8183221dbf10f56
abdulxbasit <91330477+abdulxbasit@users.noreply.github.com>
2022-07-05 13:21:41 +0500

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🚀",
+    name: "Rocket",
+    meaning:
+      "A rocket being propelled into space. Sometimes used to indicate a fast increase, Some may think that it looks astronomical and cool."
+  },
+  {
+    id: 2,
+    emoji: "🧑‍🚀",
+    name: "Astronaut",
+    meaning:
+      "An astronaut or cosmonaut shown wearing a space suit and helmet looking hella awesome"
+  },
+  {
+    id: 3,
+    emoji: "😶‍🌫️",
+    name: "Face in Clouds",
+    meaning:
+      "A face that appears surrounded by clouds or a haze of smoke. Ambiguous in appearance, this may represent a foggy state of mind, confusion, or even a sense of calm bliss."
+  },
+  {
+    id: 4,
+    emoji: "🥺",
+    name: "Pleading Face",
+    meaning:
+      "A yellow face with furrowed eyebrows, a small frown, and large, “puppy dog” eyes, as if begging or pleading. May also represent adoration or feeling touched by a loving gesture."
+  },
+  {
+    id: 5,
+    emoji: "👉👈",
+    name: "shy pose",
+    meaning:
+      "Used in sequence with pointing hands to indicate a bashful or shy pose (🥺👉👈) particularly on TikTok."
+  },
+  {
+    id: 6,
+    emoji: "🔥",
+    name: "Fire",
+    meaning:
+      "Commonly used for various metaphorical expressions related to fire, including the slang hot (“attractive”) and lit (“excellent”)."
+  }
+];
+export default emojipedia;
